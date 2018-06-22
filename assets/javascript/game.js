b3ece5e3c62ecb030d74870224e66a8a20b8881b
routhy46 <37312600+chum46@users.noreply.github.com>
2018-06-22 10:57:10 -0500

    js update with functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,7 +27,7 @@
             Unsolved_Puzzle.push("__ ");
         };
 
-        // Testing / Debugging
+        // Test in Console
 
         console.log(Puzzle_Word);
         console.log(Word_Letters);
@@ -36,14 +36,67 @@
 
         // 
         document.getElementById('puzzleWord').innerHTML=Unsolved_Puzzle.join("");
-    };
+        document.getElementById('guessesLeft').innerHTML=Guesses_Left;
+        // document.getElementById('winsCount').innerHTML=Wins_Count;
+    }
+
+    function letterCheck(guessedLetter) {
+        var checkTheLetter = false;
+        for(var i=0; i< Char_Count; i++){
+            if (Word_Letters[i]==guessedLetter) {
+                checkTheLetter = true;
+            }
+        }
+
+        if(checkTheLetter) {
+            for(var i=0; i< Char_Count; i++){
+                if (Word_Letters[i] == guessedLetter) {
+                    Unsolved_Puzzle[i]= guessedLetter;
+                    document.getElementById('puzzleWord').innerHTML=Unsolved_Puzzle;
+
+                }
+            }
+        }
+        else {
+            alert("wrong");
+            Wrong_Letters.push(guessedLetter);
+            Guesses_Left--
+            document.getElementById('guessesLeft').innerHTML=Guesses_Left;
+            document.getElementById('wrongLetters').innerHTML=Wrong_Letters;
+        }
+    }
+
+    function gameOver(){
+        if (Word_Letters.toString()==Unsolved_Puzzle.toString()){
+            Wins_Count++;
+            console.log(Wins_Count);
+            alert("Winner!");
+            document.getElementById('winsCount').innerHTML=Wins_Count;
+            gameStart();
+        }
+
+    }
+    
+    document.onkeyup = function(event) {
+        var key_Event= String.fromCharCode(event.keyCode).toLowerCase();
+        letterCheck(key_Event);
+        gameOver();
+        // Test in Console
+        console.log(key_Event);
+    }
+
+    
     
     // Store what letter is pressed in a local variable keyPress
-    // Cycle Word_Letters array to see if keyPress matches 
-    // Store the index numbers in an array
+    // Decrease Guesses_Left by 1
+    // Cycle Word_Letters array to see if keyPress matches any character 
+    // Store the index numbers of the characters that match
     // Replace blanks in Unsolved_Puzzle with the keyPress letter
     // at the appropriate index number
-    // Run this function till Unsolved_Puzzle has no more blanks
+    // Run this function till Unsolved_Puzzle has no more blanks or 
+    // if keyPress doesn't match any letter add to the end of Wrong_Letters
+    // When puzzle is solved increment Wins 
+    // or if no more guesses, prompt Game Over and restart game
     
     // FUNCTION CALLOUTS
     gameStart();
